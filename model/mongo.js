fbb30f59405cec9011a1f97791681b4a1327980c
nobody <dev@either.sch>
2017-12-16 23:08:00 +0100

    Geri - azért az öngyulladós érezhetően fosabb

---
+++ model/mongo.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/either');
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var Question = new mongoose.Schema({
+    _id: ObjectId,
+    question: String,
+    answer1: String,
+    answer2: String,
+    rating: Number,
+    status: String
+});
+
+mongoose.model('Question', Question);
+
+var Answer = new mongoose.Schema({
+    _id: {
+        type: ObjectId,
+        default: function f() {
+            return new mongoose.Types.ObjectId();
+        }
+    },
+    question: ObjectId,
+    answer: Number,
+    timestamp: Date,
+    user: String
+});
+
+mongoose.model('Answer', Answer);
+
+module.exports = mongoose.models;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
+var _ = require('../public/assets/js/lodash.js')
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/question', function (req, res, next) {
+    dal.Question.findOne(function (err, doc) {
+        res.json(doc);
+    });
+});
+
+router.post('/answer', function (req, res) {
+   new dal.Answer({question: req.body.question, answer: req.body.answer, timestamp: new Date()}).save(function (err, doc) {
+       dal.Answer.find({ question: doc.question }, function (err, doc) {
+           var ans1 = _.filter(doc, (ans) => ans.answer === 1).length;
+           var ans2 = _.filter(doc, (ans) => ans.answer === 2).length;
+           var ret = 0;
+           if(ans1 == 0 && ans2 == 0) {
+             ret = -1;
+           } else if(ans2 == 0) {
+             ret = 1;
+           } else {
+             ret = ans1 / (ans1 + ans2);
+           }
+           res.json({percent: ret});
+       });
+   })
 });
 
 module.exports = router;

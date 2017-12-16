19bcffd7e2e4c496e8b7918edf6af0797c4c22b1
kiralybalint <kiraly.balint@simonyi.bme.hu>
2017-12-16 18:33:44 +0100

    Kircsi - My first commit

---
+++ questions.js
@@ -0,0 +1,79 @@
+var questions = [
+  {
+    question: "Melyik lennél inkább?",
+    answer1: "Nagymester",
+    answer2: "Simonyi elnök"
+  },{
+    question: "Melyik kör vezetője lennél?",
+    answer1: "Kerékpár Kör",
+    answer2: "Kir-Dev"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Kék",
+    answer2: "Piros"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fekete",
+    answer2: "Sárga"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Piros",
+    answer2: "Fehér"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fekete",
+    answer2: "Fehér"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fekete",
+    answer2: "Kék"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fekete",
+    answer2: "Sárga"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fekete",
+    answer2: "Piros"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fehér",
+    answer2: "Kék"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fehér",
+    answer2: "Piros"
+  },{
+    question: "Melyik gárdába tartoznál inkább?",
+    answer1: "Fehée",
+    answer2: "Sárga"
+  },{
+    question: "Mi az idegesítőbb?",
+    answer1: "Amikor 3 lift megy el mire megjön a tiéd",
+    answer2: "XX-et dob"
+  },{
+    question: "Mit játszanál inkább?",
+    answer1: "Sörpong",
+    answer2: "Pöccpong"
+  },{
+    question: "Mit játszanál inkább?",
+    answer1: "Pöckölés",
+    answer2: "Sörpong"
+  },{
+    question: "Mit választanál?",
+    answer1: "Örök élet",
+    answer2: "Grafika kredit"
+  },{
+    question: "Mi lennél inkább?",
+    answer1: "Egyetemi oktató",
+    answer2: "Sch portás"
+  },{
+    question: "Honnan rendelnél inkább?",
+    answer1: "Dzsájrosz",
+    answer2: "Lángosch"
+  },{
+    question: "Honnan rendelnél inkább?",
+    answer1: "Americano",
+    answer2: "Pizzásch"
+  },
+]

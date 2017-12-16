f2a0dacbb394bfd98c0cfdc5eb45ae38bc07f9db
nobody <dev@either.sch>
2017-12-16 19:28:46 +0100

    Dotty - Volt szén a dohányboltban :)

--- questions.js
+++ questions.js
@@ -75,5 +75,173 @@ var questions = [
     question: "Honnan rendelnél inkább?",
     answer1: "Americano",
     answer2: "Pizzásch"
+  },{
+    question: "Honnan rendelnél inkább?",
+    answer1: "Americano",
+    answer2: "Vödör"
+  },{
+    question: "Honnan rendelnél inkább?",
+    answer1: "Vödör",
+    answer2: "Lángosch"
+  },{
+    question: "Mi lennél inkább?",
+    answer1: "Csicskagyász",
+    answer2: "SPOT körvez"
+  },{
+    question: "Mit választanál?",
+    answer1: "Hogy megvágj egy videót",
+    answer2: "Hogy minden nap kapj értesítő e-mailt arról, hogy meg kéne vágni egy videót"
+  },{
+    question: "Mit használnál inkább?",
+    answer1: "Apple",
+    answer2: "Linux"
+  },{
+    question: "Mit használnál inkább?",
+    answer1: "Apple",
+    answer2: "Windows"
+  },{
+    question: "Mit használnál inkább?",
+    answer1: "Linux",
+    answer2: "Windows"
+  },{
+    question: "Mit csinálnál inkább?",
+    answer1: "Egyenesben végeznél az egyetemmel",
+    answer2: "Csúsznál az egyetemmel, de mellette dolgoznál"
+  },{
+    question: "Melyik?",
+    answer1: "Tab",
+    answer2: "Space"
+  },{
+    question: "Melyik kellemetlenebb?",
+    answer1: "Vizsgán bealudni",
+    answer2: "Vizsgán kimenni wcre"
+  },{
+    question: "Melyik a jobb?",
+    answer1: "Hétről hétre tanulni a félév során",
+    answer2: "Egy estével a vizsga előtt mindent átnézni"
+  },{
+    question: "Mi a rosszabb?",
+    answer1: "Gólyaként sírni az analízis szigorlat miatt",
+    answer2: "Negyedévesként sírni az analízis szigorlat miatt"
+  },{
+    question: "Hogy dönténél?",
+    answer1: "Bsc után Msc",
+    answer2: "Bsc után munka"
+  },{
+    question: "Melyiket válasznád?",
+    answer1: "Mezítláb egyetemre menni",
+    answer2: "Szandál zokniban egyetemre menni"
+  },{
+    question: "Hova jársz szívesebben?",
+    answer1: "AK",
+    answer2: "Spar"
+  },{
+    question: "Hol eszel szívesebben?",
+    answer1: "Gyrososnál",
+    answer2: "Kínaiban"
+  },{
+    question: "Melyik?",
+    answer1: "La fiesta",
+    answer2: "Borbár"
+  },{
+    question: "Melyiket választanád?",
+    answer1: "Grafika 2-es",
+    answer2: "Adatbé 2-es"
+  },{
+    question: "Mit választanál?",
+    answer1: "Tábor rendezés",
+    answer2: "FNT takker"
+  },{
+    question: "Melyik?",
+    answer1: "Rendezés",
+    answer2: "Főrendezés"
+  },{
+    question: "Milyen ember vagy?",
+    answer1: "Kutyás",
+    answer2: "Macskás"
+  },{
+    question: "Melyik?",
+    answer1: "Albérlet egyedül",
+    answer2: "Koli haverokkal"
+  },{
+    question: "Melyik?",
+    answer1: "Sör",
+    answer2: "Cider"
+  },{
+    question: "Milyen pia?"
+    answer1: "Rövid",
+    answer2: "Hosszú"
+  },{
+    question: "Mire jársz?",
+    answer1: "Villany",
+    answer2: "Infó"
+  },{
+    question: "Melyik nehezebb?",
+    answer1: "Villany",
+    answer2: "Infó"
+  },{
+    question: "Miben hiszel?",
+    answer1: "Lapos Föld",
+    answer2: "Gyíkemberek"
+  },{
+    question: "Melyik?",
+    answer1: "Használni végtelen javascript frameworköt egy projekthez",
+    answer2: "Megírni egy saját frameworköt egy projekthez"
+  },{
+    question: "Mit iszol inkább?",
+    answer1: "Tea",
+    answer2: "Kávé"
+  },{
+    question: "Mit ennél szívesen egy táborban?",
+    answer1: "Csilisbab",
+    answer2: "Csilisbab"
+  },{
+    question: "Feketék?",
+    answer1: "Nincs baj velük, mindenkinek kellene egy",
+    answer2: "Hol?"
+  },{
+    question: "Melyik?",
+    answer1: "Igen",
+    answer2: "Nem"
+  },{
+    question: "Melyik lifttel utazol szívesebben?",
+    answer1: "A",
+    answer2: "B"
+  },{
+    question: "Melyik lifttel utazol szívesebben?",
+    answer1: "A",
+    answer2: "C"
+  },{
+    question: "Melyik lifttel utazol szívesebben?",
+    answer1: "A",
+    answer2: "D"
+  },{
+    question: "Melyik lifttel utazol szívesebben?",
+    answer1: "B",
+    answer2: "C"
+  },{
+    question: "Melyik lifttel utazol szívesebben?",
+    answer1: "B",
+    answer2: "D"
+  },{
+    question: "Melyik lifttel utazol szívesebben?",
+    answer1: "C",
+    answer2: "D"
+  },{
+    question: "Ki?",
+    answer1: "Katalin",
+    answer2: "Gréti"
+  },{
+    question: "Melyik a nagyobb bullshit?",
+    answer1: "Soft skillek fejlesztlése",
+    answer2: "Professzionális játszótér"
+  },{
+    question: "Mi a jobb?",
+    answer1: "2.5-ös átlaggal fuckolni, hogy a seniorok bemutyizzák magukat a koliba",
+    answer2: "Bemutyizni magad a koliba"
+  },{
+    question: "",
+    answer1: "",
+    answer2: ""
   },
 ]

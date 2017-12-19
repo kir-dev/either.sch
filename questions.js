a447b7c830e13e9ac8b36888c01d19fda9f945d2
nobody <dev@either.sch>
2017-12-19 20:13:04 +0100

    Geri - ne nézegessétek a repot RVT chat!

--- questions.js
+++ questions.js
@@ -4,50 +4,6 @@ var questions = [
     answer1: "Nagymester",
     answer2: "Simonyi elnök"
   },{
-    question: "Melyik kör vezetője lennél?",
-    answer1: "Kerékpár Kör",
-    answer2: "Kir-Dev"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Kék",
-    answer2: "Piros"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fekete",
-    answer2: "Sárga"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Piros",
-    answer2: "Fehér"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fekete",
-    answer2: "Fehér"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fekete",
-    answer2: "Kék"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fekete",
-    answer2: "Sárga"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fekete",
-    answer2: "Piros"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fehér",
-    answer2: "Kék"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fehér",
-    answer2: "Piros"
-  },{
-    question: "Melyik gárdába tartoznál inkább?",
-    answer1: "Fehér",
-    answer2: "Sárga"
-  },{
     question: "Mi az idegesítőbb?",
     answer1: "Amikor 3 lift megy el mire megjön a tiéd",
     answer2: "XX-et dob"
@@ -84,10 +40,6 @@ var questions = [
     answer1: "Vödör",
     answer2: "Lángosch"
   },{
-    question: "Mi lennél inkább?",
-    answer1: "Csicskagyász",
-    answer2: "SPOT körvez"
-  },{
     question: "Mit választanál?",
     answer1: "Hogy megvágj egy videót",
     answer2: "Hogy minden nap kapj értesítő e-mailt arról, hogy meg kéne vágni egy videót"
@@ -119,10 +71,6 @@ var questions = [
     question: "Melyik a jobb?",
     answer1: "Hétről hétre tanulni a félév során",
     answer2: "Egy estével a vizsga előtt mindent átnézni"
-  },{
-    question: "Mi a rosszabb?",
-    answer1: "Gólyaként sírni az analízis szigorlat miatt",
-    answer2: "Negyedévesként sírni az analízis szigorlat miatt"
   },{
     question: "Hogy dönténél?",
     answer1: "Bsc után Msc",
@@ -196,10 +144,6 @@ var questions = [
     answer1: "Csilisbab",
     answer2: "Csilisbab"
   },{
-    question: "Feketék?",
-    answer1: "Nincs baj velük, mindenkinek kellene egy",
-    answer2: "Hol?"
-  },{
     question: "Melyik?",
     answer1: "Igen",
     answer2: "Nem"
@@ -232,21 +176,9 @@ var questions = [
     answer1: "Katalin",
     answer2: "Gréti"
   },{
-    question: "Melyik a nagyobb bullshit?",
-    answer1: "Soft skillek fejlesztlése",
-    answer2: "Professzionális játszótér"
-  },{
-    question: "Mi a jobb?",
-    answer1: "2.5-ös átlaggal fuckolni, hogy a seniorok bemutyizzák magukat a koliba",
-    answer2: "Bemutyizni magad a koliba"
-  },{
     question: "Mit tennél inkább?",
     answer1: "Minden héten hazamennél",
     answer2: "Csak ünnepekkor mennél haza"
-  },{
-    question: "Mi a rosszabb?",
-    answer1: "Az egyik liftben ragadni pár órára",
-    answer2: "Vitázni a portásokkal"
   },{
     question: "Mit csinálnál inkább?",
     answer1: "Lépcsőzni a 3.-ra",
